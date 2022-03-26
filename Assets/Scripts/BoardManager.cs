dc3011b74801e3845b6da14f04992448c9175f6a
Juan <juan.dgb.99@gmail.com>
2022-03-25 23:59:43 -0400

    skulls movement

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BoardManager
+{
+
+	[System.Serializable]
+	public struct rowData
+	{
+		public bool[] row;
+	}
+
+	public Grid grid;
+	public rowData[] rows = new rowData[14]; //Grid of 7x7
+}
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,80 +5,185 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public enum PieceType
-    {
-        Normal,
-        Count,
-    }
-    [System.Serializable]
-    public struct PiecePrefab
-    {
-        public PieceType type;
-        public GameObject prefab;
-    }
-    public int gridX = 9;
-    public int gridY = 9;
+    public BoardManager boardManager;
+    public Sprite[] pieces;
+    int width = 9;
+    int height = 14;
+    Node[,] board;
 
-    public PiecePrefab[] piecePrefabs;
-    public GameObject backgroundPrefab;
-
-    private Dictionary<PieceType, GameObject> piecePrefabDict;
-    private Skulls[,] pieces;
+    System.Random random;
 
     void Start()
     {
-        piecePrefabDict = new Dictionary<PieceType, GameObject>();
+        
+    }
+    void StartGame()
+    {
+        string seed = getRandomSeed();
+        random = new System.Random(seed.GetHashCode());
 
-        for (int i = 0; i < piecePrefabs.Length; i++)
+        InitializeBoard();
+    }
+
+    void InitializeBoard()
+    {
+        board = new Node[width, height];
+
+        for (int y = 0; y < height; y++)
         {
-            if (!piecePrefabDict.ContainsKey(piecePrefabs[i].type))
+            for (int x = 0; x < width; x++)
             {
-                piecePrefabDict.Add(piecePrefabs[i].type, piecePrefabs[i].prefab);
-            }
-        }
-
-        for (int x = 0; x < gridX; x++)
-        {
-            for (int y = 0; y < gridY; y++)
-            {
-                GameObject background = (GameObject)Instantiate(backgroundPrefab, GetWorldPosition(x, y), Quaternion.identity);
-                background.transform.parent = transform;
-            }
-        }
-
-        pieces = new Skulls[gridX, gridY];
-        for (int x = 0; x < gridX; x++)
-        {
-            for (int y = 0; y < gridY; y++)
-            {
-                GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.Normal], Vector3.zero, Quaternion.identity);
-                newPiece.name = "Piece(" + x + "," + y + ")";
-                newPiece.transform.parent = transform;
-
-                pieces[x, y] = newPiece.GetComponent<Skulls>();
-                pieces[x, y].Init(x, y, this, PieceType.Normal);
-
-                if (pieces [x, y].IsMovable())
-                {
-                    pieces[x, y].MovableComponent.Move(x, y);
-                }
-
-                if (pieces[x, y].IsColored())
-                {
-                    pieces[x, y].SkullsComponent.SetColor((SkullsColor.ColorType)Random.Range(0, pieces[x, y].SkullsComponent.NumColors));
-                }
+                board[x, y] = new Node((boardManager.rows[y].row[x]) ? -1 : fillPiece(), new SlotBehaviour(x, y));
             }
         }
     }
 
-    // Update is called once per frame
+    void VerifyBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                SlotBehaviour p = new SlotBehaviour(x, y);
+                int val = getValueAtPoint(p);
+                if (val <= 0) continue;
+
+
+            }
+        }
+    }
+
+    List<SlotBehaviour> isConnected(SlotBehaviour p, bool main)
+    {
+        List<SlotBehaviour> connected = new List<SlotBehaviour>();
+        int val = getValueAtPoint(p);
+        SlotBehaviour[] directions =
+        {
+            SlotBehaviour.up,
+            SlotBehaviour.right,
+            SlotBehaviour.down,
+            SlotBehaviour.left
+        };
+
+
+
+        foreach (SlotBehaviour dir in directions) //checks for 2 or more same skulls in the directions
+        {
+            List<SlotBehaviour> line = new List<SlotBehaviour>();
+
+            int same = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                SlotBehaviour check = SlotBehaviour.add(p, SlotBehaviour.mult(dir, i));
+                if (getValueAtPoint(check) == val)
+                {
+                    line.Add(check);
+                    same++;
+                }
+            }
+
+            if(same > 1) // if there are more than 1 of the same shape in the direction then its a match
+            {
+                AddPoints(ref connected, line); //adds the points to the overarching connected list
+            }
+        }
+
+        for (int i = 0; i < 2; i++) //checking if we are in the middle of two of the same shapes
+        {
+            List<SlotBehaviour> line = new List<SlotBehaviour>();
+
+            int same = 0;
+            SlotBehaviour[] check = { SlotBehaviour.add(p, directions[i]), SlotBehaviour.add(p, directions[i + 2]) };
+            
+            foreach(SlotBehaviour next in check) //check both sides of the piece, adds them to the list if they are the same
+            {
+                if (getValueAtPoint(next) == val)
+                {
+                    line.Add(p);
+                    same++;
+                }
+            }
+
+            if (same > 1)
+            {
+                AddPoints(ref connected, line);
+            }
+        }
+        for (int i = 0; i < 4; i++) //check for 2x2
+        {
+            List<SlotBehaviour> square = new List<SlotBehaviour>();
+
+            int same = 0;
+            int next = i + 1;
+            if (next >= 4)
+            {
+                next -= 4;
+
+                SlotBehaviour[] check = { SlotBehaviour.add(p, directions[i]), SlotBehaviour.add(p, directions[next]), SlotBehaviour.add(p, SlotBehaviour.add(directions[i], directions[next])};
+                foreach (SlotBehaviour pnt in check) //check both sides of the piece, adds them to the list if they are the same
+                {
+                    if (getValueAtPoint(pnt) == val)
+                    {
+                        square.Add(p);
+                        same++;
+                    }
+                }
+                if (same > 2)
+                {
+                    AddPoints(ref connected, square);
+                }
+            }
+            if (main)
+            {
+
+            }
+        }
+    }
+
+    void AddPoints(ref List<SlotBehaviour> points, List<SlotBehaviour> add)
+    {
+
+    }
+
+    int fillPiece()
+    {
+        int val = 1;
+        val = (random.Next(0, 100) / (100 / pieces.Length)) + 1;
+        return val;
+    }
+
+    int getValueAtPoint(SlotBehaviour p)
+    {
+        return board[p.x, p.y].value;
+    }
+
     void Update()
     {
         
     }
-
-    public Vector3 GetWorldPosition(int x, int y)
+    string getRandomSeed()
     {
-        return new Vector3(transform.position.x - gridX / 2.0f + x, transform.position.y + gridY / 2.0f - y);
+        string seed = "";
+        string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()";
+        
+        for (int i = 0; i < 20; i++)
+        {
+            seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+        }
+
+        return seed;
+    }
+}
+
+[System.Serializable]
+public class Node
+{
+    public int value;
+    public SlotBehaviour index;
+
+    public Node(int v, SlotBehaviour i)
+    {
+        value = v;
+        index = i;
     }
 }
